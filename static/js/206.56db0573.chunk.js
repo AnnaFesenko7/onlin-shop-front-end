b681a78f9128a7ff7fd1da53afb6051945b50273
AnnaFesenko7 <90966976+AnnaFesenko7@users.noreply.github.com>
2023-08-31 17:22:33 +0000

    Deploying to gh-pages from @ AnnaFesenko7/onlin-shop-front-end@2bc1acef86ac8b96e9d3b326c37e462a06b33770 🚀

---
+++ static/js/206.56db0573.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkonlin_shop_front_end=self.webpackChunkonlin_shop_front_end||[]).push([[206],{206:function(n,e,o){o.r(e);var r=o(184);e.default=function(){return(0,r.jsx)("h1",{children:"HomePage"})}}}]);
+//# sourceMappingURL=206.56db0573.chunk.js.map
